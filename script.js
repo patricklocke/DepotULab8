b24a03e1855123735b14b60906ded51ce7ac83c9
Patrick Locke <patrick.s.locke@gmail.com>
2016-03-07 11:33:24 -0600

    added functionality to roll individual dice and delete on double click

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-var Die = function()  {
-    this.value = null;
-    this.div = null;
-    this.insert = function (){
+var Die = function () {
+    this.insert = function () {
         this.div = document.createElement('div');
+        this.div.id = dice.length;
+        $(this.div).click(function(){
+            var newnum = Math.floor(Math.random()*6)+1;
+            this.innerHTML = newnum;
+        });
+        $(this.div).on('dblclick', function(){
+           $(this).remove();
+        });
         this.div.className = 'dice';
         this.roll();
         document.getElementById('dice-container').appendChild(this.div);
-    
     }
-    this.roll = function() {
-        var num = Math.floor(Math.random()*6) + 1;
+    this.roll = function () {
+        var num = Math.floor(Math.random() * 6) + 1;
         this.value = num;
         this.div.innerHTML = num;
     }
 }
 
+
 var dice = [];
 var sum = []
-
-
-function add(a,b){
+function add(a, b) {
     return a + b;
 }
-
-
 function addDie() {
     var die = new Die();
     die.insert();
     dice.push(die);
 }
-
 function rollDice() {
     for (var i = 0; i < dice.length; i++) {
         dice[i].roll();
     }
 }
-
 function sumDice() {
-    for (var i=0; i < dice.length; i++) {
-        var values = dice[i].value
+    for (var i = 0; i < dice.length; i++) {
+        var values = dice[i].value;
         sum.push(values);
     }
-        var total = sum.reduce(add, 0);
-        alert("The dice total is "+total); 
+    var total = sum.reduce(add, 0);
+    alert("The dice total is " + total);
+    dice = []
 }
+
