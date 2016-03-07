35231814c8c8c5cdc6a933bc1b98875c1e7cb2c0
Patrick Locke <patrick.s.locke@gmail.com>
2016-03-07 10:19:56 -0600

    Fixed function for dice roll, and added sum button.

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-$(document).ready(function(){
+var Die = function()  {
+    this.value = null;
+    this.div = null;
+    this.insert = function (){
+        this.div = document.createElement('div');
+        this.div.className = 'dice';
+        this.roll();
+        document.getElementById('dice-container').appendChild(this.div);
+    
+    }
+    this.roll = function() {
+        var num = Math.floor(Math.random()*6) + 1;
+        this.value = num;
+        this.div.innerHTML = num;
+    }
+}
 
-var Die = function (value, roll){
-   var newDie = function (){
-       $('.container').append("<div class='dice'></div>")
-   };
-   newDie.value = (Math.floor(Math.random()*6)+1),
-   $( '<p>'+newDie.value+'</p>').appendTo('newDie'),
-   console.log(newDie.value),
-   newDie()
- };
-
-$("#addDie").on('click', function() {
-    Die();
-});
-$('#roll').on('click', function () {
-   $('#dice').value=(Math.floor(Math.random()*6)+1)
-});
+var dice = [];
+var sum = []
 
 
-});
+function add(a,b){
+    return a + b;
+}
+
+
+function addDie() {
+    var die = new Die();
+    die.insert();
+    dice.push(die);
+}
+
+function rollDice() {
+    for (var i = 0; i < dice.length; i++) {
+        dice[i].roll();
+    }
+}
+
+function sumDice() {
+    for (var i=0; i < dice.length; i++) {
+        var values = dice[i].value
+        sum.push(values);
+    }
+        var total = sum.reduce(add, 0);
+        alert("The dice total is "+total); 
+}
